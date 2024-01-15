a03832fe8b9464d6794f6f90ca60a43b3bda933c
Casimir Lundberg <clundber@c1r2p3.hive.fi>
2024-01-15 17:59:58 +0200

    Different alowed functions tested

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 11:34:07 by clundber          #+#    #+#             */
-/*   Updated: 2024/01/13 17:46:00 by clundber         ###   ########.fr       */
+/*   Updated: 2024/01/15 17:18:48 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,42 @@ void	arg_check(int argc, char **argv)
 	close(fd);
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envv)
 
 {
 	int	pid;
+	int fd;
+
+	arg_check(argc, argv);
+	
+	// fork training
+	int package;
+	package = 0;
 	int	pipe1[2];
 	int	pipe2[2];
-	int package;
-
-	package = 10;
-	arg_check(argc, argv);
-	// fork training
 	pipe(pipe1);
 	pipe(pipe2);
 	pid = fork();
+
+	if(execve(argv[2]/*array?*/, argv[1]/*array*/, envv/*array*/) == -1)
+	{
+		perror("could not execute command\n");
+
+	}
+
 	if (pid == 0) // this is the child process
 	{
-		read(pipe1, &foo_copy.txt, size)
-		ft_printf("Hello, this is child\n");
+		read(pipe1[0], &package, sizeof(int));
+		fd = open (argv[4], O_WRONLY);
+		dup2(fd, 1);
+		//ft_printf("Hello, this is child\n");
+		ft_printf("the package number is %d\n", package);
+		close(fd);
 	}
 	else // this is the parent process
 	{
-		write(pipe1, &foo_copy.txt, size);
+		package = 10;
+		write(pipe1[1], &package, sizeof(int));
 		//wait(&pid);
 		ft_printf("and I am parent\n");
 	}
