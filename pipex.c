3c68a03f5689013078c40433d6e520ced42edb39
Casimir Lundberg <clundber@c1r4p5.hive.fi>
2024-01-23 15:03:02 +0200

    commands incuding quotes working

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: welhox <welhox@student.42.fr>              +#+  +:+       +#+        */
+/*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 11:34:07 by clundber          #+#    #+#             */
-/*   Updated: 2024/01/19 15:12:34 by welhox           ###   ########.fr       */
+/*   Updated: 2024/01/23 15:01:09 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,18 @@ void	arg_check(int argc, char **argv)
 {
 	int	fd;
 
-	if (argc != 5)// || !argv[2] || !argv[3])
+	if (argc != 5)
 	{
 		write(2, "Usage is : ./pipex file1 cmd1 cmd2 file2\n", 41);	
 		exit (1);
 	}
+
 	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
+ 	if (fd < 0)
 	{
 		perror("infile");
-		//ft_printf("unable to open read file\n");
-		exit (1);
-	}
-	close(fd);
-/* 	fd = open (argv[4], O_WRONLY);
-	if (fd < 0)
-	{
-		perror("Error");
-		exit (1);
-	} */
+		exit (0);
+	} 
 	close(fd);
 }
 
@@ -99,12 +92,18 @@ void	child_one(t_pipex *pipex)
 {
 	int	fd;
 	fd = open(pipex->argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror(pipex->argv[1]);
+		write(pipex->pipe_fd[1], "", 0);
+		exit(1);
+	}
 	dup2(fd, 0);
 	close(pipex->pipe_fd[0]);
 	dup2(pipex->pipe_fd[1], 1);
 	if(execve(pipex->path, pipex->cmd_array, pipex->envp) == -1)
-		perror("could not execute command\n");
-
+		//perror("could not execute command\n");
+		write(2, "could not execute command\n", 26);
 }
 
 void	child_two(t_pipex *pipex)
@@ -117,12 +116,13 @@ void	child_two(t_pipex *pipex)
 		ft_free_all(pipex);
 		perror("outfile");
 		exit(1);
-	}	
+	}
 	dup2(fd, 1);
 	close(pipex->pipe_fd[1]);
 	dup2(pipex->pipe_fd[0], 0);
 	if(execve(pipex->path2, pipex->cmd_array2, pipex->envp) == -1)
-		perror("could not execute command\n");
+		write(2, "could not execute command\n", 26);
+		//perror("could not execute command\n");
 }
 
 void	ft_free_str(char *str)
@@ -144,6 +144,43 @@ void	ft_free_all(t_pipex *pipex)
 	ft_free(pipex->cmd_array2);
 } 
 
+char	**ft_splitter(char *str)
+
+{
+	char **temp_array;
+
+	temp_array = NULL;
+
+
+	int	i;
+	int k;
+
+	k = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == 39 || str[i] == 34)
+		{
+			temp_array = ft_split(str, str[i]);
+			while (temp_array[0][k])
+			{
+				if (temp_array[0][k] == ' ')
+				{	
+					temp_array[0] = ft_substr(temp_array[0], 0, k);
+					break; 
+				}
+				k++;
+			}	
+			break;
+		}	
+		i++;
+	}
+	if (!temp_array)
+		temp_array = ft_split(str, ' ');
+	return(temp_array);
+
+}
+
 int	main(int argc, char *argv[], char *envp[])
 
 {
@@ -155,14 +192,19 @@ int	main(int argc, char *argv[], char *envp[])
 	pid = 0;
 	pid2 = 0;
 	//argc = 0;
-	arg_check(argc, argv);
+	if (argc != 5)
+	{
+		write(2, "Usage is : ./pipex file1 cmd1 cmd2 file2\n", 41);	
+		exit (1);
+	}
+	//arg_check(argc, argv);
 	pipe(pipe_fd);
 	pipex.argv = argv;
 	pipex.envp = envp;
 	pipex.pipe_fd[0] = pipe_fd[0];
 	pipex.pipe_fd[1] = pipe_fd[1];
-	pipex.cmd_array = ft_split(argv[2], ' ');
-	pipex.cmd_array2 = ft_split(argv[3], ' ');
+	pipex.cmd_array = ft_splitter(argv[2]);
+	pipex.cmd_array2 = ft_splitter(argv[3]);
 	pipex.path = get_path(pipex.cmd_array[0], envp);
 	pipex.path2 = get_path(pipex.cmd_array2[0], envp);
 	pid = fork();
@@ -175,97 +217,15 @@ int	main(int argc, char *argv[], char *envp[])
 	if (pid == 0)
 		child_one(&pipex);
 	if (pid != 0)
+	{
+		wait(&pid);
 		wait(&pid2);
+	}
 	close(pipex.pipe_fd[0]);
 	close(pipex.pipe_fd[1]);
 	ft_free_all(&pipex);
 	return(0);
 }
-
-
-/* 	if (pid != 0) //parent
-	{
-		cmd_arr = ft_split(argv[2], ' ');
-		fd = open(argv[1], O_RDONLY);
-		dup2(fd, 0);
-		close(pipe_fd[0]);
-		dup2(pipe_fd[1], 1);
-		path = get_path(cmd_arr[0], envp);
-		if(execve(path, cmd_arr, envp) == -1)
-		{
-		perror("could not execute command\n");
-		}
-	}
-	else // child
-	{
-		cmd_arr = ft_split(argv[3], ' ');
-		fd = open(argv[4], O_WRONLY);
-		dup2(fd, 1);
-		close(pipe_fd[1]);
-		dup2(pipe_fd[0], 0);
-		path = get_path(cmd_arr[0], envp);
-		if(execve(path, cmd_arr, envp) == -1)
-		{
-		perror("could not execute command\n");
-		}
-	} */
-
-// 	path = get_path(argv[2], envp);
-	//argc = 0; // for silencing the warning, DELETE when argcheck returned
-/*	//arg_check(argc, argv);
-
-
-
-	fd1 = open (argv[3], O_WRONLY);
-	dup2(fd1, 1);
-
-
-  	if(execve(path, cmd_arr, envp) == -1)
-	{
-		perror("could not execute command\n");
-
-	}
-		// fork traini */
-
-/* 	int i = 0;
-	while(envp[i])
-	{
-	ft_printf("the envp = %s\n\n", envp[i]);
-	i++;
-	} */
-
-	//char cmd[] = "bin/wc";
-//	char *argv[] = {"/bin/ls", "-l", NULL};
-//	char *envp[] = {NULL};
-
-/* 	int package;
-	package = 0;
-	int	pipe1[2];
-	int	pipe2[2];
-	pipe(pipe1);
-	pipe(pipe2); */
-	//pid = fork();
-/*
-	if (pid == 0) // this is the child process
-	{
-		read(pipe1[0], &package, sizeof(int));
-		fd = open (argv[4], O_WRONLY);
-		dup2(fd, 1);
-		//ft_printf("Hello, this is child\n");
-		ft_printf("the package number is %d\n", package);
-		close(fd);
-	}
-	else // this is the parent process
-	{
-		package = 10;
-		write(pipe1[1], &package, sizeof(int));
-		//wait(&pid);
-		ft_printf("and I am parent\n");
-	} */
-
-	// fork training
-
-
 /*
 open, close, read, write,
 malloc, free, perror,
